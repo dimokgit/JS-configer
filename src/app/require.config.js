c4f3170231a6646c8dedfc70fc2f77b903ac0a2e
dimokgit <dimokhot@hotmail.com>
2015-05-20 14:27:58 -0400

    Added app-config

--- src/app/require.config.js
+++ src/app/require.config.js
@@ -2,18 +2,10 @@
 var require = {
   baseUrl: ".",
   paths: {
-    "bootstrap": "bower_modules/components-bootstrap/js/bootstrap.min",
-    "crossroads": "bower_modules/crossroads/dist/crossroads.min",
-    "hasher": "bower_modules/hasher/dist/js/hasher.min",
     "jquery": "bower_modules/jquery/dist/jquery",
     "knockout": "bower_modules/knockout/dist/knockout",
-    "knockout-projections": "bower_modules/knockout-projections/dist/knockout-projections",
-    "signals": "bower_modules/js-signals/dist/signals.min",
-    "text": "bower_modules/requirejs-text/text",
     "configer": "app/configer",
+    "app-config": "app/app-config",
     "promise-monad": "bower_modules/monads/lib/promise-monad"
-  },
-  shim: {
-    "bootstrap": { deps: ["jquery"] }
   }
 };
--- src/app/startup.js
+++ src/app/startup.js
@@ -1,30 +1,22 @@
 /// <reference path="../bower_modules/knockout/dist/knockout.js" />
 
-define(['jquery', 'knockout', './router', 'configer', 'bootstrap', 'knockout-projections'], function ($, ko, router, configer) {
+define(['knockout', 'configer', 'app-config'], function (ko, configer, appConfig) {
+  // set SPA configuration
+  appConfig.configerUrl = "http://usmrtpwiisp01:9000/api/configer/";
+  appConfig.dataServiceUrl = "FinanceDataService.svc";
+  appConfig.appName = "ibar";
 
-  // Components can be packaged as AMD modules, such as the following:
-  ko.components.register('nav-bar', { require: 'components/nav-bar/nav-bar' });
-  ko.components.register('home-page', { require: 'components/home-page/home' });
-
-  // ... or for template-only components, you can just point to a .html file directly:
-  ko.components.register('about-page', {
-    template: { require: 'text!components/about-page/about.html' }
-  });
-
-  // [Scaffolded component registrations will be inserted here. To retain this feature, don't remove this comment.]
-  function toDictionary(o) {
-    return $.map(o, function (v, n) { return { key: n, value: ko.unwrap(v) }; });
-  }
-  // Start the application
-  var vm = { configer: [], error: "" };
+  // fetch back-end configuration from HPS
+  var vm = { configer: "", error: "" };
   configer
+    .fetch(appConfig)
     .done(function (c) {
-      $.extend(vm, { configer: toDictionary(c) });
+      $.extend(vm, { configer: JSON.stringify(c, null, 2) });
     })
     .fail(function (e) {
-      $.extend(vm, { error: JSON.stringify(e,null,2) });
+      $.extend(vm, { error: JSON.stringify(e, null, 2) });
     }).always(function () {
       ko.applyBindings(vm);
     });
   ;
-});
+});;
