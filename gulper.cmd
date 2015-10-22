98bd20bf7aba94532ccbcc41c4ceed75b5ac7fb2
dimokgit <dimokhot@hotmail.com>
2015-10-22 16:29:15 -0400

    eee

--- gulper.cmd
+++ gulper.cmd
@@ -1,13 +1,15 @@
 title configer
 cd %~d0
 cd %~p0
-set path=%path%;C:\Users\ipbldit\AppData\Roaming\npm
-
-rem @echo off
-set /p msg="commit message: "
+set path=%path%;%APPDATA%\npm
 
 call gulp
 
 
+rem @echo off
+set /p msg="commit message: "
 git commit -a -m %msg%
+
+git push
+
 pause
